8edf91968f0bfa528747af4ecb298e4bd075fc47
Edoardo Tenani <edoardo.tenani@pm.me>
2022-09-06 18:06:00 +0200

    infra(dev): enable on demand scanning

--- infra/dev/main.tf
+++ infra/dev/main.tf
@@ -16,7 +16,17 @@ module "cluster" {
   source = "../modules/cluster"
 }
 
-# NOTE: no need for registry, is already enabled and we don't need Container Scanning at the moment.
+# NOTE: no need for registry, is already enabled
+resource "google_project_service" "project" {
+  # enabling this API automatically enables on demand vulnerability scanning
+  # We scan on demand from CI pipelines after building images to prevent possible issues
+  # when pushing to the staging repo in production project
+  service = "ondemandscanning.googleapis.com"
+
+  disable_dependent_services = false
+  # NOTE: this settings will require manual disabling of the API after destroy
+  disable_on_destroy = false
+}
 
 output "gcp_project_id" {
   value = data.vault_generic_secret.dev.data["gcp_project_id"]
